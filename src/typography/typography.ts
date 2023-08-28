50aad6522af2adb6c3b16bd750cebd0ba4f03105
Débora Carvalho <debora.sesv@gmail.com>
2023-08-27 21:35:24 -0300

    Styleguide typography in progress

---
+++ src/typography/typography.ts
@@ -0,0 +1,29 @@
+export const typography = {
+  family: {
+    primary: "Source Sans Pro",
+  },
+
+  lineHeight: {
+    xxxxLarge: "36px",
+    xxxLarge: "26px",
+    xxLarge: "24px",
+    xLarge: "21px",
+    large: "18px",
+    medium: "14px",
+    small: "12px"
+  },
+
+  fontSize: {
+    xxLarge: "24px",
+    XLarge: "18px",
+    large: "16px",
+    medium: "14px",
+    small: "12px"
+  },
+
+  fontWeight: {
+    bold: 700,
+    semiBold: 600,
+    regular: 400
+  },
+};
