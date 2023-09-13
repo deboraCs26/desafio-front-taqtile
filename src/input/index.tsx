3821982d80122ce26bca1ca48c3ae38f24954566
Débora Carvalho <debora.sesv@gmail.com>
2023-09-13 17:04:31 -0300

    Task styleguide input

--- src/input/index.tsx
+++ src/input/index.tsx
@@ -1,8 +1,31 @@
 import styled from 'styled-components';
-import { CustomInput } from './style';
+import { InputProps } from './style';
+import { colors } from '../colors/colors';
 
-// Crie um componente Styled para o input
-export const StyledInput = styled.input`
-  width: 100%;
-  /* Adicione seus estilos personalizados aqui */
+export const StyledInput = styled.input<InputProps>`
+  width: ${({ expand }) => (expand ? '100%' : 'calc(100% - 25px)')};
+  border-radius: 10px;
+  border: 1px solid var(--base-gray-light, #99a2ab);
+  background: var(--base-background, #f9f9f9);
+  outline: none;
+  padding: 12px 20px;
+  margin-top: 8px;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+  margin: 20px;
+`;
+
+export const IconContainer = styled.div`
+  position: absolute;
+  top: 35%;
+  right: 5px;
+  color: ${colors.base.gray};
+`;
+
+export const RelativeContainer = styled.div`
+  position: relative;
 `;
--- src/input/style.tsx
+++ src/input/style.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { StyledInput } from '.';
+import { InputContainer, StyledInput, RelativeContainer, IconContainer } from '.';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { Label } from '../typography/label';
 
-export const CustomInput = ({ label, value, onChange, onFocus, onBlur, password }) => {
+export interface InputProps {
+  label?: string;
+  icon?: IconDefinition;
+  password?: boolean;
+  expand?: boolean;
+  placeholder?: string | undefined;
+  className?: string;
+}
+
+export const CustomInput = ({ label, password, placeholder, icon }: InputProps) => {
+  const inputId = `custom-input-${label ?? 'default'}`;
+
   return (
-    <StyledInput
-      type={password ? 'password' : 'text'}
-      placeholder={label}
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      onFocus={onFocus}
-      onBlur={onBlur}
-      required
-    />
+    <InputContainer>
+      <Label htmlFor={inputId}>{label}</Label>
+      <RelativeContainer>
+        <StyledInput
+          type={password ? 'password' : 'text'}
+          required
+          id={inputId}
+          {...(placeholder && { placeholder })}
+        />
+        <IconContainer>
+          {icon && <FontAwesomeIcon icon={icon} size="lg" style={{ transform: 'rotate(180deg)' }} />}
+        </IconContainer>
+      </RelativeContainer>
+    </InputContainer>
   );
 };
