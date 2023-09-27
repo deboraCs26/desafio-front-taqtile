3af6b75a1bea53107b69cbf434fbc8f09912f3e2
Débora Carvalho <debora.sesv@gmail.com>
2023-09-26 21:37:44 -0300

    task styleguide form

---
+++ src/form/style.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const IconForm = styled.div`
+  padding-right: 8px;
+  display: inline-block;
+`;
+
+export const ErrorForm = styled.div`
+  padding-top: 8px;
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
